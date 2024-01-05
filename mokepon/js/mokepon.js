af23390ea88f9681130b7e9efcb3b4683f922f45
Hector Cedeno <hectord17@gmail.com>
2024-01-05 18:48:45 -0500

    Clases y objetos de Mokepon

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -26,6 +26,23 @@ let ataqueEnemigo
 let vidasJugador = 3
 let vidasEnemigo = 3
 
+//Las clases inician con mayúculas.
+class Mokepon {
+    constructor(nombre, foto, vida) {
+        this.nombre = nombre
+        this.foto = foto
+        this.vida = vida
+    }
+}
+
+//Con la clase Mokepon creada arriba, se crean los tres objetos (mokepones)
+let hipodoge = new Mokepon('Hipodoge', './assets/mokepons_mokepon_hipodoge_attack.png', 5)
+
+let capipepo = new Mokepon('Capipepo', './assets/mokepons_mokepon_capipepo_attack.png', 5)
+
+let ratigueya = new Mokepon('Ratigueya', './assets/mokepons_mokepon_ratigueya_attack.png', 5)
+
+
 //Primero se crean las funciones que se van a ejecutar cuando cargue toda la página.
 
 function iniciarJuego(){
